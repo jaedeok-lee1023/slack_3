3a11597c6d6d2c7b479a51773707e937ebad963e
jaedeok-lee1023 <jaedeok.lee_b@kurlycorp.com>
2026-01-02 13:27:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -48,25 +48,29 @@ def send_slack_message(message, channel):
 def main():
     for cluster in clusters:
         # 메시지 제목 설정
-        header = f":loudspeaker: *『인사총무팀 공지』* <!channel>\n\n"
+        header = f"*[공지｜클러스터 주차등록 및 이용 안내]*\n\n\n"
 
         notice_msg = (
-            f"안녕하세요? 평택 클러스터 구성원 여러분\n\n올바른 주차등록과 주차장 이용을 위해 구성원 여러분들께\n\n"
-            f"다음과 같이 공지 드리오니 꼭 협조 부탁드리겠습니다.\n\n"
+            f"1. *중요도* : 중\n"
+            f"2. *대상* : 평택 클러스터 임직원 전체\n"
+            f"3. *주요 내용*\n\n"
             f"\n"
-            f":k체크: 주차 등록 시 *첨부자료* 참고하여 설문조사 후 *2층 통합사무실에서 주차증* 수령\n"
+            f"안녕하세요? 평택 클러스터 구성원 여러분!\n\n"
+            f"올바른 주차장 이용과 신규 주차 등록을 위해 공지 드리오니\n"
+            f"아래 내용 참고하시어, 이용 부탁드리겠습니다.\n\n"
+            f"\n"
+            f":k체크: 주차 등록 시 *첨부자료* 참고하여 설문조사 후 *2층 인사총무팀에서 주차증* 수령\n"
             f"(설문조사 내 본인차량 *자동차등록증* 준비 및 *증빙자료* 첨부 必)\n\n"
             f"\n"
             f":k체크: *8층 주차장 외* 주차 확인 될 경우 *주차 위반 스티커 부착* 등 조치 예정\n"
             f":arrow_forward: 주차장 주차라인 내 주차 준수 / 경차 및 전기차 전용 준수\n"
-            f":arrow_forward: 지자체에서 외부 불법 주/정차 수시 단속 중\n"
-            f":arrow_forward: 부속동 주차장 이용 시 직원식당 측에서 견인 등 조치 예정\n\n"
+            f":arrow_forward: 외부 불법 주/정차 수시 단속 진행 중_관공서\n"
+            f":arrow_forward: 부속동(직원식당) 주차장 주차불가, 주차위반 스티커 또는 견인 조치 예정\n\n"
             f"\n"
             f"*:k체크: <https://docs.google.com/forms/d/e/1FAIpQLSfK121pYkYfmsL9I9MGX5tgA8Y6u1v03ZOP2RX9stmaLGH_Sg/viewform|주차등록링크>* :point_left: *(Click)*\n"
-            f"*:k체크: <https://static.wixstatic.com/media/50072f_8d974c0474ba4ba5b8652ffd237c6a9b~mv2.jpg|주차등록 및 주차장 안내>*\n"
             f"\n"
             f"*자세한 자료는 :point_up: :point_up: 클릭하여 확인 부탁드립니다!*\n\n"
-            f"*문의사항 : 인사총무팀 총무/시설 담당자*\n\n"
+            f"*:slack: 문의사항 : 인사총무팀 총무/시설 담당자*\n\n"
             f"감사합니다.\n"
         )
  
